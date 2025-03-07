808313c8b04ab6c3a3014b643ae8caefbfddae13
laurajacint0 <lyjacint@uci.edu>
2025-03-07 22:48:56 +0000

    changed print total when no path found

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -46,6 +46,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.empty()) {
+        cout << "\nTotal cost is " << total << endl;
         cout << "No path found." << endl;
     } else {
         for (size_t i = 0; i < path.size(); ++i) {
