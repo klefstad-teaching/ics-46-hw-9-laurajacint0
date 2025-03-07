0bcee40e62235f5cd73e962286508ad7014bc931
laurajacint0 <lyjacint@uci.edu>
2025-03-07 22:33:42 +0000

    fixed ladder print format

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -69,8 +69,8 @@ void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
         cout << "No word ladder found." << endl;
     } else {
+        cout << "Word ladder found: ";
         for (size_t i = 0; i < ladder.size(); ++i) {
-            cout << "Word ladder found: ";
             cout << ladder[i];
             if (i < ladder.size() - 1) cout << " ";
         }
